50eff23631c0c37b522eace279d0ca323a2d805e
sambhavraj71 <sambhavraj83@gmail.com>
2026-08-09 16:09:02 +0530

    Fix the cross origin

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ load_dotenv()
 
 
 origins = [
-    "https://codenexus-nodt.onrender.com/",  
+    "https://codenexus-nodt.onrender.com",
     "http://localhost:5173",
     "http://localhost:3000",
 ]
