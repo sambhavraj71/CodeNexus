aac35ae92ef614a80315be7eee80830c56e7dd7a
Sambhav Raj <sambhavraj83@gmail.com>
2026-08-09 08:17:19 +0530

    Update API URL to include production endpoint

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -8,7 +8,8 @@ import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 function Login() {
     const navigate = useNavigate();
-    const API = "http://127.0.0.1:8000";
+    const API = "http://127.0.0.1:8000 || https://codenexus-backend-0we9.onrender.com
+";
     const [isLogin, setIsLogin] = useState(true);
     const [showPassword, setShowPassword] = useState(false);
     const [formData, setFormData] = useState({
